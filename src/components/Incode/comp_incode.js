cc3a0218f7c1629d637d6963e3414d79b183aa6f
Matias Azcui <matias.azcui@gmail.com>
2022-04-04 22:35:48 -0300

    chau webpack christopher te odio

--- src/components/Incode/comp_incode.js
+++ src/components/Incode/comp_incode.js
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { Modal, Spinner, Row, Col } from 'react-bootstrap';
-import { label_ } from '../../styles/leters';
-import '../../css/Incode.css';
-import { incodeServices } from '../../services/incode.services';
+import React, { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import { Modal, Spinner, Row, Col } from "react-bootstrap"
+import { label_ } from "../../styles/leters"
+import "../../css/Incode.css"
+import { incodeServices } from "../../services/incode.services"
 
-let onBoarding;
-let session;
+let onBoarding
+let session
 
-const container = document.getElementById('camera-container');
+const container = document.getElementById("camera-container")
 
 export default function COMP_Incode() {
   function createOnBoarding() {
     /* PRD  */
-    const apiKey = '64993050ce0ea4c9273ba590e92005a1cea69e34';
-    const apiURL = 'https://saas-api.incodesmile.com/';
+    const apiKey = "64993050ce0ea4c9273ba590e92005a1cea69e34"
+    const apiURL = "https://saas-api.incodesmile.com/"
 
     /* DEV
   const apiURL = 'https://demo-api.incodesmile.com/';
@@ -23,103 +23,108 @@ export default function COMP_Incode() {
     return window.OnBoarding.create({
       apiKey,
       apiURL,
-      lang: 'es'
-    });
+      lang: "es",
+    })
   }
 
   function createSession() {
-    return onBoarding.createSession('ALL');
+    return onBoarding.createSession("ALL")
   }
 
   function showError() {
-    alert('Ah ocurrido al procesar la solicitud');
+    alert("Ah ocurrido al procesar la solicitud")
   }
 
   async function renderFrontTutorial() {
     onBoarding.renderFrontTutorial(container, {
       onSuccess: renderFrontIDCamera,
-      token: session
-    });
+      token: session,
+    })
   }
 
   async function renderFrontIDCamera() {
-    await Send_Zenziya_update();
+    await Send_Zenziya_update()
 
-    onBoarding.renderCamera('front', container, {
+    onBoarding.renderCamera("front", container, {
       onSuccess: renderBackTutorial,
       onError: showError,
       token: session,
-      numberOfTries: 3
-    });
+      numberOfTries: 3,
+    })
   }
 
   async function renderBackTutorial() {
     onBoarding.renderBackTutorial(container, {
       onSuccess: renderBackIDCamera,
-      token: session
-    });
+      token: session,
+    })
   }
 
   function renderBackIDCamera() {
-    onBoarding.renderCamera('back', container, {
+    onBoarding.renderCamera("back", container, {
       onSuccess: processId,
       onError: showError,
       token: session,
-      numberOfTries: 3
-    });
+      numberOfTries: 3,
+    })
   }
 
   function processId() {
-    container.innerHTML = '<p>Loading...</p>';
+    container.innerHTML = "<p>Loading...</p>"
     onBoarding.processId({ token: session.token }).then(() => {
-      container.innerHTML = '';
-      renderSelfieCamera();
-    });
+      container.innerHTML = ""
+      renderSelfieCamera()
+    })
   }
 
   function renderSelfieCamera() {
-    onBoarding.renderCamera('selfie', container, {
+    onBoarding.renderCamera("selfie", container, {
       onSuccess: () => {
-        alert('Felicidades has completado todo el proceso.');
-        incodeServices.notifyEndProcess(searchParams.get('EntityIdOnboarding'));
+        alert("Felicidades has completado todo el proceso.")
+        incodeServices.notifyEndProcess(searchParams.get("EntityIdOnboarding"))
       },
       onError: showError,
       token: session,
-      numberOfTries: 3
-    });
+      numberOfTries: 3,
+    })
   }
 
-  const [ini, setIni] = useState(true);
-  const [message] = useState('Cargando..');
-  const [searchParams] = useSearchParams();
+  const [ini, setIni] = useState(true)
+  const [message] = useState("Cargando..")
+  const [searchParams] = useSearchParams()
 
   function Send_Zenziya_update() {
     var data = {
       interviewId: session.interviewId,
       interviewCode: session.interviewCode,
       token: session.token,
-      entityIdOnboarding: searchParams.get('EntityIdOnboarding')
-    };
-    incodeServices.notifyBeginProcess(data);
+      entityIdOnboarding: searchParams.get("EntityIdOnboarding"),
+    }
+    incodeServices.notifyBeginProcess(data)
     alert(
-      'Comenzaste el proceso cuyo ID es: ' +
-        searchParams.get('EntityIdOnboarding')
-    );
+      "Comenzaste el proceso cuyo ID es: " +
+        searchParams.get("EntityIdOnboarding")
+    )
   }
 
-  useEffect(async () => {
-    try {
-      onBoarding = createOnBoarding(); // initialize the instance
-      await onBoarding.warmup();
-      const _session = await createSession();
-      console.log(_session);
-      session = _session;
-      await renderFrontTutorial(); // render and start autodetect of the front ID camera
-      setIni(false);
-    } catch (error) {
-      console.log(error.message);
+  useEffect(() => {
+    async function doSomething() {
+      try {
+        debugger
+        onBoarding = createOnBoarding() // initialize the instance
+        await onBoarding.warmup()
+        const _session = await createSession()
+        console.log(_session)
+        session = _session
+        await renderFrontTutorial() // render and start autodetect of the front ID camera
+        setIni(false)
+      } catch (error) {
+        console.log(error.message)
+      }
     }
-  }, []);
+
+    doSomething() //
+  }, [])
 
   return (
     <Modal show={ini} centered>
@@ -132,5 +137,5 @@ export default function COMP_Incode() {
         </Row>
       </Modal.Body>
     </Modal>
-  );
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import ReactDom from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Vw_Privacidad from './layout/Onbording';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Vw_Privacidad from "./layout/Onbording"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
-ReactDom.render(
+const root = ReactDOM.createRoot(document.getElementById("z"))
+root.render(
   <Router>
     <Routes>
       <Route path="/" element={<Vw_Privacidad />} />
     </Routes>
-  </Router>,
-  document.getElementById('z')
-);
+  </Router>
+)
